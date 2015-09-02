5e7ed8e11360b617657d49e7454ca63df085d426
Johnny Groat <johnathan.groat@gmail.com>
2015-09-01 23:38:35 -0400

    Add Rectangle Models and Newtonsoft JSON Deserializer

---
+++ Rectangles/Models/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rectangles.Models
+{
+    public class Rectangle
+    {
+        public int xVertex1 { get; set; }
+        public int yVertex1 { get; set; }
+
+        public int xVertex2 { get; set; }
+        public int yVertex2 { get; set; }        
+    }
+
+    public class RectanglePair
+    {
+        public Rectangle rectangle1 { get; private set; }
+        public Rectangle rectangle2 { get; private set; }
+
+        public List<RectangleProperties> rectangleProperties { get; set; }
+
+        public RectanglePair(Rectangle rect1, Rectangle rect2)
+        {
+            rectangle1 = rect1;
+            rectangle2 = rect2;
+        }
+
+
+    }
+
+    public enum RectangleProperties
+    {
+        Intersection = 0,
+        Containment,
+        Adjacency
+    }
+}
