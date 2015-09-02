c672018a11ed92c4e5c9ce1645e32c92cd4357e6
Johnny Groat <johnathan.groat@gmail.com>
2015-09-02 02:03:58 -0400

    build out vertex comparison logic

--- Rectangles/Controllers/HomeController.cs
+++ Rectangles/Controllers/HomeController.cs
@@ -19,7 +19,49 @@ namespace Rectangles.Controllers
         {
             RectanglePair rectanglePair = new RectanglePair(Newtonsoft.Json.JsonConvert.DeserializeObject<Rectangle>(rectangle1Vertices), Newtonsoft.Json.JsonConvert.DeserializeObject<Rectangle>(rectangle2Vertices));
 
-            return View();
+            RectangleProperties pairProperties = new RectangleProperties();
+
+            pairProperties.hasContainment = HasContainment(rectanglePair);
+            pairProperties.hasAdjacency = HasAdjacency(rectanglePair);
+            pairProperties.hasIntersection = (pairProperties.hasContainment ? false : HasIntersection(rectanglePair));
+            
+
+            rectanglePair.rectangleProperties = pairProperties;
+            return Json(rectanglePair);
+        }
+
+        public bool HasIntersection(RectanglePair rectanglePair)
+        {
+            if (rectanglePair.rectangle1.xVertex1 < rectanglePair.rectangle2.xVertex2 && rectanglePair.rectangle1.xVertex2 > rectanglePair.rectangle2.xVertex1 && rectanglePair.rectangle1.yVertex1 > rectanglePair.rectangle2.yVertex2 && rectanglePair.rectangle1.yVertex2 < rectanglePair.rectangle2.yVertex1)
+                return true;
+            return false;
+        }
+
+        public bool HasAdjacency(RectanglePair rectanglePair)
+        {
+            //Right alignment
+            if (rectanglePair.rectangle1.xVertex2 == rectanglePair.rectangle2.xVertex1 && ((rectanglePair.rectangle1.yVertex2 >= rectanglePair.rectangle2.yVertex2 && rectanglePair.rectangle1.yVertex2 <= rectanglePair.rectangle2.yVertex1) ||(rectanglePair.rectangle1.yVertex1 >= rectanglePair.rectangle2.yVertex2 && rectanglePair.rectangle1.yVertex1 <= rectanglePair.rectangle2.yVertex1)))
+            {
+                return true;
+            }
+            //Top alignment
+            else if (rectanglePair.rectangle1.yVertex1 == rectanglePair.rectangle2.yVertex2 && ((rectanglePair.rectangle1.xVertex2 >= rectanglePair.rectangle2.xVertex1 && rectanglePair.rectangle1.xVertex2 <= rectanglePair.rectangle2.xVertex2) || (rectanglePair.rectangle1.xVertex1 >= rectanglePair.rectangle2.xVertex1 && rectanglePair.rectangle1.xVertex1 <= rectanglePair.rectangle2.xVertex2)))
+            {
+                return true;
+            }
+            //Bottom alignment
+            else if (rectanglePair.rectangle1.yVertex2 == rectanglePair.rectangle2.yVertex1 && ((rectanglePair.rectangle1.xVertex2 >= rectanglePair.rectangle2.xVertex1 && rectanglePair.rectangle1.xVertex2 <= rectanglePair.rectangle2.xVertex2) || (rectanglePair.rectangle1.xVertex1 >= rectanglePair.rectangle2.xVertex1 && rectanglePair.rectangle1.xVertex1 <= rectanglePair.rectangle2.xVertex2)))
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool HasContainment(RectanglePair rectanglePair)
+        {
+            if (rectanglePair.rectangle1.xVertex1 <= rectanglePair.rectangle2.xVertex1 && rectanglePair.rectangle1.yVertex1 >= rectanglePair.rectangle2.yVertex1 && rectanglePair.rectangle1.xVertex2 >= rectanglePair.rectangle2.xVertex2 && rectanglePair.rectangle1.yVertex2 <= rectanglePair.rectangle2.yVertex2)
+                return true;
+
+            return false;
         }
     }
 }
